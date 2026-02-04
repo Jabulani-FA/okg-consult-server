beb5886ca2e43a51caeb9824ddf4351cd7c84dc8
Olorunfemi Akanbi <akanbi398@gmail.com>
2026-02-04 17:08:01 +0100

    error corrected with exit

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.get("/", (req, res) => {
 //   }
 // });
 const mailRoutes = require("./routes/mail");
+const userRoutes = require("./routes/user");
 app.use("/api", mailRoutes);
+app.use("/api", userRoutes)
 
 module.exports = app;
