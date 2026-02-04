41899391ce6b35a60696de609fb4d35d7e75a5af
Olorunfemi Akanbi <akanbi398@gmail.com>
2026-02-04 17:15:53 +0100

    error corrected with exit

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-const app = require('./app');
+// const app = require('./app');
 
-const port = process.env.PORT || 4000;
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs");
+require("dotenv").config();
+const app = express();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://okgconsult.com",
+  "https://www.okgconsult.com",
+];
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    optionsSuccessStatus: 200,
+  }),
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("OKG Consult Backend is running");
+});
+
+// Dynamically load all routes from the routes directory
+fs.readdirSync("./routes").forEach((file) => {
+  if (file.endsWith(".js")) {
+    const route = require(`./routes/${file}`);
+    app.use("/api", route);
+  }
+});
+// const mailRoutes = require("./routes/mail");
+// const userRoutes = require("./routes/user");
+// app.use("/api", mailRoutes);
+// app.use("/api", userRoutes)
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- routes/gallery.js
+++ routes/gallery.js
@@ -4,10 +4,10 @@ const {authenticateToken} = require("../middleware/auth");
 const { getGallery, addImage, deleteImage } = require("../controllers/gallery");
 
 // Get all gallery images with limit and offset
-router.get("/", getGallery);   
+router.get("/gallery", getGallery);   
 // Add a new image to the gallery (protected route)
-router.post("/", authenticateToken, addImage);
+router.post("/gallery", authenticateToken, addImage);
 // Delete an image from the gallery (protected route)
-router.delete("/:id", authenticateToken, deleteImage);
+router.delete("/gallery/:id", authenticateToken, deleteImage);
 
 module.exports = router;
